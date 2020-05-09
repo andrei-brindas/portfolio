f89a2a6b822b9d36793c411f14b84d29383e92f7
Andrei <andrei.brindas97@gmail.com>
2020-05-10 00:52:58 +0300

    download CV

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} className="button" download>
+                  <a href={resumeDownload} className="button">
                     <i className="fa fa-download" ></i>Download Resume
                   </a>
                 </p>
